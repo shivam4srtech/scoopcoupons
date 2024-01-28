26df698ccf921f5d16c107ec5c3d7c4bbc91cf51
shivam4srtech <shivam.srtech@gmail.com>
2024-01-28 22:19:38 +0530

    first commit

---
+++ scoopcoupons/scripts/app.js
@@ -0,0 +1,39 @@
+// SEARCH FORM
+function input() {
+  var x = document.getElementById("input");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+    
+    
+    
+    function share() {
+      var x = document.getElementById("share");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
+    function email() {
+        var x = document.getElementById("email");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+      function comment() {
+        var x = document.getElementById("comment");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
